2c2024a687db80160d924241fb4fe32f0777c292
asahutchinson4 <60147025+asahutchinson4@users.noreply.github.com>
2020-06-10 13:27:16 -0500

    Added death
    Enemy can die and healthbar will dissappear

---
+++ Kid Flame/Assets/Scripts/Enemies/BasicEnemyController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BasicEnemyController : MonoBehaviour
+{
+   private enum State
+    {
+        Moving,
+        Dead
+    }
+    
+    public GameObject healthBar;
+    public Transform barPoint;
+
+    private State currentState;
+
+    [SerializeField]
+    private float
+        groundCheckDistance,
+        wallCheckDistance,
+        movementSpeed;
+
+    [SerializeField]
+    private Transform
+        groundCheck,
+        wallCheck;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+
+    private int
+        facingDirection;
+        
+
+    private Vector2 movement;
+
+    private bool
+        groundDetected,
+        wallDetected;
+
+    private GameObject alive;
+    private Rigidbody2D aliveRb;
+    private Animator aliveAnim;
+
+    private void Start()
+    {
+        alive = transform.Find("Alive").gameObject;
+        aliveRb = alive.GetComponent<Rigidbody2D>();
+        aliveAnim = alive.GetComponent<Animator>();
+
+
+        facingDirection = 1;
+
+        carryHealthBar();
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Moving:
+                UpdateMovingState();
+                break;
+        }
+
+        healthBar.transform.position = barPoint.transform.position;
+    }
+
+    private void UpdateMovingState()
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if(!groundDetected || wallDetected)
+        {
+            flip();
+        }
+        else
+        {
+            movement.Set(movementSpeed * facingDirection, aliveRb.velocity.y);
+            aliveRb.velocity = movement;
+        }
+    }
+
+    private void flip()
+    {
+        facingDirection *= -1;
+        alive.transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void carryHealthBar()
+    {
+        healthBar = Instantiate(healthBar, barPoint.position, barPoint.rotation);
+    }
+}
---
+++ Kid Flame/Assets/Scripts/Enemies/takeDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class takeDamage : MonoBehaviour
+{
+    public EnemyHealth health;
+    public Sprite Hurt;
+    public Sprite Local;
+   
+    float timer = 1f;
+    float delay = 1f;
+  
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+
+    if (col.gameObject.CompareTag("Bullet"))
+        {
+            health.ReduceHealth(15f);
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Hurt;
+            timer = delay;
+            return;
+        }
+        
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Local;
+        }
+
+        if(health.currentHealth == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
