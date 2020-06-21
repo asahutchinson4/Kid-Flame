054a3c67d1e64231e684331f8391292978cbb713
asahutchinson4 <60147025+asahutchinson4@users.noreply.github.com>
2020-06-21 12:13:28 -0500

    FireFighter stops
    The FireFighter enemy stops when he sees kidFlame. Need to make detection radius bigger. Next objective is to make firefighter shoot water at kidflame when he is detected. Also healthbar still flips when enemies flip.

--- Kid Flame/Assets/Scripts/FireFighters/FireFighterController.cs
+++ Kid Flame/Assets/Scripts/FireFighters/FireFighterController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class FireFighterController : MonoBehaviour
@@ -22,16 +25,21 @@ public class FireFighterController : MonoBehaviour
         groundCheckDistance,
         wallCheckDistance,
         carCheckDistance,
+        kidCheckDistance,
         movementSpeed;
 
     [SerializeField]
     private Transform
         groundCheck,
         wallCheck,
-        carCheck;
+        carCheck,
+        kidCheck;
 
     [SerializeField]
     private LayerMask whatIsGround;
+
+    [SerializeField]
+    private LayerMask whatIsKidFlame;
 
 
     private int
@@ -39,11 +47,12 @@ public class FireFighterController : MonoBehaviour
 
 
     private Vector2 movement;
-
+ 
     private bool
         groundDetected,
         wallDetected,
-        carDetected;
+        carDetected,
+        kidDetected;
 
     private GameObject fireMan;
     private Rigidbody2D fireManRb;
@@ -78,8 +87,15 @@ public class FireFighterController : MonoBehaviour
         groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
         wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
         carDetected = Physics2D.Raycast(carCheck.position, transform.right, carCheckDistance, whatIsGround);
+        kidDetected = Physics2D.Raycast(kidCheck.position, transform.right, kidCheckDistance, whatIsKidFlame);
 
-        if (!groundDetected || wallDetected || carDetected)
+        if (kidDetected)
+        {
+            UnityEngine.Debug.Log("Hit");
+            stop();
+        }
+
+        else if (!groundDetected || wallDetected || carDetected)
         {
             flip();
         }
@@ -96,11 +112,18 @@ public class FireFighterController : MonoBehaviour
         fireMan.transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 
+    private void stop()
+    {
+        movement.Set(movementSpeed * 0f, fireManRb.velocity.y);
+        fireManRb.velocity = movement;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
         Gizmos.DrawLine(carCheck.position, new Vector2(carCheck.position.x + carCheckDistance, carCheck.position.y));
+        Gizmos.DrawLine(kidCheck.position, new Vector2(kidCheck.position.x + kidCheckDistance, kidCheck.position.y));
     }
 
     //public void carryHealthBar()
---
+++ Kid Flame/Assets/Scripts/Management/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform targetToFollow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(targetToFollow.position.x, -25f, 1000f),
+            transform.position.y,
+            transform.position.z);
+        
+    }
+}
