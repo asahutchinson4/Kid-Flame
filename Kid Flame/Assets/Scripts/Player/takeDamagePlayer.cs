a834bd5677b3dbbf178af7f0590ae0ef4ad33346
asahutchinson4 <60147025+asahutchinson4@users.noreply.github.com>
2020-06-27 12:58:47 -0500

    Player Health
    Added art for a hurt animation for kidflame. Gave him health system and ability to take damage when hit by water

---
+++ Kid Flame/Assets/Scripts/Player/takeDamagePlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class takeDamagePlayer : MonoBehaviour
+{
+
+    public characterHealth health;
+    public Sprite Shot;
+    public Sprite Idle2;
+    public Sprite Cold;
+
+    float timer = 1f;
+    float delay = 1f;
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.CompareTag("Water"))
+        {
+            health.ReduceHealth(5f);
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Cold;
+            timer = delay;
+            return;
+        }
+    }
+   
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Shot;
+            SoundManager.playFireballSound();
+            timer = delay;
+            return;
+        }
+
+        if (timer <= 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Idle2;
+        }
+
+        if (health.currentHealth == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
