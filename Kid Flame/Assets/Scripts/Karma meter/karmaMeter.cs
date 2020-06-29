e658aacbfaf7149f0f762ad77d5b2e9664d7e14a
asahutchinson4 <60147025+asahutchinson4@users.noreply.github.com>
2020-06-28 20:28:38 -0500

    Karma Meter
    Added karma meter. Top right of the screen there is a karma meter that points to either good or evil. Need to add counter so the pointing will depend on how many people you've killed

---
+++ Kid Flame/Assets/Scripts/Karma meter/karmaMeter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class karmaMeter : MonoBehaviour {
+
+    private const float MAX_BAD = -174;
+    private const float MAX_GOOD = 174;
+
+    private Transform needleTransform;
+
+    private float moralMax;
+    private float moral;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        needleTransform = transform.Find("needle");
+
+        moral = 0f;
+        moralMax = 200f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moral += 30f * Time.deltaTime;
+        
+        if(moral > moralMax)
+        {
+            moral = moralMax;
+        }
+
+        needleTransform.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
+    }
+
+    private float GetSpeedRotation()
+    {
+        float totalAngleSize = MAX_GOOD - MAX_BAD;
+
+        float moralNormalized = moral / moralMax;
+
+        return MAX_GOOD - moralNormalized * totalAngleSize;
+    }
+}
