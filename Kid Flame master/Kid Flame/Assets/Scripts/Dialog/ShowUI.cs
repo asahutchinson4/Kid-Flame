1573f16b19530bd93df5a49d1d8d12fc524551b3
asahutchinson4 <60147025+asahutchinson4@users.noreply.github.com>
2020-07-18 11:02:02 -0500

    More Dialog
    Added more Dialog. Will work on later today

--- Kid Flame master/Kid Flame/Assets/Scripts/Dialog/ShowUI.cs
+++ Kid Flame master/Kid Flame/Assets/Scripts/Dialog/ShowUI.cs
@@ -6,12 +6,20 @@ public class ShowUI : MonoBehaviour
 {
     public GameObject KidIntro;
     public GameObject DadGreeting;
+    public GameObject DadApology;
+    public GameObject KidResponseToApology;
+    public GameObject DadAdmit;
+    public GameObject KidPain;
 
     // Start is called before the first frame update
     void Start()
     {
         KidIntro.SetActive(false);
         DadGreeting.SetActive(false);
+        DadApology.SetActive(false);
+        KidResponseToApology.SetActive(false);
+        DadAdmit.SetActive(false);
+        KidPain.SetActive(false);
     }
 
    public void OnTriggerEnter2D(Collider2D col)
@@ -27,6 +35,37 @@ public class ShowUI : MonoBehaviour
             StartCoroutine("FinishTalking1");
             StartCoroutine("WaitForSec2");
         }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking2");
+            StartCoroutine("WaitForSec3");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking3");
+            StartCoroutine("WaitForSec4");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking4");
+            StartCoroutine("WaitForSec5");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking5");
+            StartCoroutine("WaitForSec6");
+        }
+    }
+
+
+    IEnumerator WaitForSec1()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(DadGreeting);
     }
 
     IEnumerator FinishTalking1()
@@ -35,15 +74,57 @@ public class ShowUI : MonoBehaviour
         KidIntro.SetActive(true);
     }
 
-    IEnumerator WaitForSec1()
-    {
-        yield return new WaitForSeconds(3);
-        Destroy(DadGreeting);
-    }
-    
     IEnumerator WaitForSec2()
     {
         yield return new WaitForSeconds(7);
         Destroy(KidIntro);
+    }
+
+    IEnumerator FinishTalking2()
+    {
+        yield return new WaitForSeconds(7);
+        DadApology.SetActive(true);
+    }
+
+    IEnumerator WaitForSec3()
+    {
+        yield return new WaitForSeconds(12);
+        Destroy(DadApology);
+    }
+
+    IEnumerator FinishTalking3()
+    {
+        yield return new WaitForSeconds(12);
+        KidResponseToApology.SetActive(true);
+    }
+
+    IEnumerator WaitForSec4()
+    {
+        yield return new WaitForSeconds(18);
+        Destroy(KidResponseToApology);
+    }
+
+    IEnumerator FinishTalking4()
+    {
+        yield return new WaitForSeconds(18);
+        DadAdmit.SetActive(true);
+    }
+
+    IEnumerator WaitForSec5()
+    {
+        yield return new WaitForSeconds(24);
+        Destroy(DadAdmit);
+    }
+
+    IEnumerator FinishTalking5()
+    {
+        yield return new WaitForSeconds(24);
+        KidPain.SetActive(true);
+    }
+
+    IEnumerator WaitForSec6()
+    {
+        yield return new WaitForSeconds(28);
+        Destroy(KidPain);
     }
 }
