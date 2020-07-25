9b4c8c6c92d3dd91f0f14a67a05305aa5389cd64
asahutchinson4 <60147025+asahutchinson4@users.noreply.github.com>
2020-07-25 15:47:14 -0500

    End Screen
    Made some good progress on end screen. Gonna work on it some more after I go shoot

---
+++ Kid Flame master/Kid Flame/Assets/Scripts/Ending/LevelComplete.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+    public GameObject winScreen;
+    public GameObject killedStamp;
+    public GameObject sparedStamp;
+    public GameObject dadSprite;
+    public GameObject completionText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winScreen.SetActive(false);
+        killedStamp.SetActive(false);
+        sparedStamp.SetActive(false);
+        dadSprite.SetActive(false);
+        completionText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && Fate.enableKeyS == true)
+        {
+            StartCoroutine("WaitForNeedle");
+            StartCoroutine("DadWasSpared");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine("WaitForNeedle");
+            StartCoroutine("DadWasKilled");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+       
+    }
+
+    IEnumerator WaitForNeedle()
+    {
+        yield return new WaitForSeconds(2);
+        winScreen.SetActive(true);
+        dadSprite.SetActive(true);
+        completionText.SetActive(true);
+    }
+
+    IEnumerator DadWasKilled()
+    {
+        yield return new WaitForSeconds(4);
+        killedStamp.SetActive(true);
+    }
+
+    IEnumerator DadWasSpared()
+    {
+        yield return new WaitForSeconds(4);
+        sparedStamp.SetActive(true);
+    }
+}
---
+++ Kid Flame master/Kid Flame/Assets/Scripts/Ending/ShowUI.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+    public GameObject KidIntro;
+    public GameObject DadGreeting;
+    public GameObject DadApology;
+    public GameObject KidResponseToApology;
+    public GameObject DadAdmit;
+    public GameObject KidPain;
+    public GameObject DadBeg;
+    public GameObject KidFlame;
+
+    public PlayerMovement kidScript;
+    public characterHealth health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KidIntro.SetActive(false);
+        DadGreeting.SetActive(false);
+        DadApology.SetActive(false);
+        KidResponseToApology.SetActive(false);
+        DadAdmit.SetActive(false);
+        KidPain.SetActive(false);
+        DadBeg.SetActive(false);
+
+        kidScript = KidFlame.GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        if (health.currentHealth == 0)
+        {
+            Destroy(DadGreeting);
+            Destroy(KidIntro);
+            Destroy(DadApology);
+            Destroy(KidResponseToApology);
+            Destroy(DadAdmit);
+            Destroy(KidPain);
+            Destroy(DadBeg);
+        }
+    }
+
+   public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Kid Flame")) ;
+        {
+            kidScript.moveSpeed = 0f;
+            kidScript.jumpVelocity = 0f;
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"));
+        {
+            DadGreeting.SetActive(true);
+            StartCoroutine("WaitForSec1");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking1");
+            StartCoroutine("WaitForSec2");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking2");
+            StartCoroutine("WaitForSec3");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking3");
+            StartCoroutine("WaitForSec4");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking4");
+            StartCoroutine("WaitForSec5");
+        }
+
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking5");
+            StartCoroutine("WaitForSec6");
+        }
+        
+        if (col.gameObject.CompareTag("Kid Flame"))
+        {
+            StartCoroutine("FinishTalking6");
+            StartCoroutine("WaitForSec7");
+        }
+    }
+
+
+    IEnumerator WaitForSec1()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(DadGreeting);
+    }
+
+    IEnumerator FinishTalking1()
+    {
+        yield return new WaitForSeconds(3);
+        KidIntro.SetActive(true);
+    }
+
+    IEnumerator WaitForSec2()
+    {
+        yield return new WaitForSeconds(7);
+        Destroy(KidIntro);
+    }
+
+    IEnumerator FinishTalking2()
+    {
+        yield return new WaitForSeconds(7);
+        DadApology.SetActive(true);
+    }
+
+    IEnumerator WaitForSec3()
+    {
+        yield return new WaitForSeconds(12);
+        Destroy(DadApology);
+    }
+
+    IEnumerator FinishTalking3()
+    {
+        yield return new WaitForSeconds(12);
+        KidResponseToApology.SetActive(true);
+    }
+
+    IEnumerator WaitForSec4()
+    {
+        yield return new WaitForSeconds(18);
+        Destroy(KidResponseToApology);
+    }
+
+    IEnumerator FinishTalking4()
+    {
+        yield return new WaitForSeconds(18);
+        DadAdmit.SetActive(true);
+    }
+
+    IEnumerator WaitForSec5()
+    {
+        yield return new WaitForSeconds(24);
+        Destroy(DadAdmit);
+    }
+
+    IEnumerator FinishTalking5()
+    {
+        yield return new WaitForSeconds(24);
+        KidPain.SetActive(true);
+    }
+
+    IEnumerator WaitForSec6()
+    {
+        yield return new WaitForSeconds(28);
+        Destroy(KidPain);
+    }
+
+    IEnumerator FinishTalking6()
+    {
+        yield return new WaitForSeconds(28);
+        DadBeg.SetActive(true);
+    }
+
+    IEnumerator WaitForSec7()
+    {
+        yield return new WaitForSeconds(32);
+        Destroy(DadBeg);
+    }
+}
